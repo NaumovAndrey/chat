57d317a93c903b9f13d7c686c33e9f6b10b28cab
andrey <naumov.a.n@icloud.com>
2023-11-13 22:03:35 +0700

    Добавлены коментарии ServerGUI

--- src/main/java/ru/gb/server/ServerGUI.java
+++ src/main/java/ru/gb/server/ServerGUI.java
@@ -6,10 +6,12 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 public class ServerGUI extends JFrame {
+    //region Константы
     public static final int POSITION_X = 800;
     public static final int POSITION_Y = 300;
     public static final int WINDOW_WIDTH = 800;
     public static final int WINDOW_HEIGHT = 500;
+    //endregion
 
     JButton btnExit;
     JToggleButton btnStart;
@@ -22,6 +24,14 @@ public class ServerGUI extends JFrame {
         setLocation(POSITION_X, POSITION_Y);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
+        //region Текстовое поле. Ввод с клавиатуры запрещён.
+        textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        add(scrollPane);
+        textArea.setEditable(false);
+        //endregion
+
+        //region Кнопка "Включить", запускает сервер. Название кнопки изменяется на отключить.
         btnStart = new JToggleButton("Включить");
         btnStart.addItemListener(new ItemListener() {
             @Override
@@ -33,13 +43,20 @@ public class ServerGUI extends JFrame {
                 }
             }
         });
+        //todo Реализовать действие по нажатию кнопки "Включить/выключить"
+        //endregion
 
+        //region Кнопка "Выход"
         btnExit = new JButton("Выход");
+        //todo Реализовать действие по нажатию кнопки "выход"
+        //endregion
 
+        //region Создана панель, в панель добавлено две кнопки "Выход и подключить/отключить"
         JPanel panBotton = new JPanel(new GridLayout(1, 2));
         panBotton.add(btnStart);
         panBotton.add(btnExit);
         add(panBotton, BorderLayout.SOUTH);
+        //endregion
 
         setVisible(true);
     }
